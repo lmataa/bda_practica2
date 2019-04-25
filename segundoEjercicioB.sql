cfbda94fe93cb84e123a930d436954a901ce9a05
Javier Morate del Moral <37208022+javimorate@users.noreply.github.com>
2019-04-25 19:11:58 +0200

    segundoEjercicioB bien hecho

--- segundoEjercicioB.sql
+++ segundoEjercicioB.sql
@@ -1,11 +1,12 @@
 SET GLOBAL query_cache_type = 0;
 use practica2;
 select t.team_id,teamName,SUM(tshots) as tiros,AVG(tgoals) as gol_partido,count(*) as numPartidos
-from team t, team_stats ts, game g 
-where t.team_id=ts.team_id and ts.game_id=g.game_id and date_time between '2017-07-1' and '2017-12-31'
+from team t inner join team_stats ts on t.team_id=ts.team_id 
+	inner join game g on ts.game_id=g.game_id
+where year(date_time)=2017 and month(date_time)>=7 
 group by t.team_id, teamName
 having numPartidos >= ALL( select count(*) as numPartidos
 							from team t inner join team_stats ts on t.team_id=ts.team_id 
 									inner join game g on ts.game_id=g.game_id
-							where date_time between '2017-07-1' and '2017-12-31'
+							where year(date_time)=2017 and month(date_time)>=7 
                             group by t.team_id);
