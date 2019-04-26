9074536aca0cc0edc75862fca077bd132d5ed94a
Javier Morate del Moral <37208022+javimorate@users.noreply.github.com>
2019-04-26 19:47:32 +0200

    triggers a falta de revision

---
+++ triggers.sql
@@ -0,0 +1,20 @@
+create trigger update_asistencias after update
+on player_stats for each row
+update player
+set asistencias=asistencias+new.assists-old.assists
+where player.player_id=player_id;
+
+
+create trigger update2_asistencias after insert
+on player_stats for each row
+update player 
+set asistencias=asistencias+new.assists
+where player.player_id=new.player_id ;
+
+
+create trigger update3_asistencias before delete
+on player_stats for each row
+update player
+set asistencias=asistencias-old.assists
+where player.player_id=old.player_id;
+
